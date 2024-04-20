7d68fceccda873a6ce3ece4869c59e66cebbcfc3
Haewon Seok <114521565+haewonseok@users.noreply.github.com>
2024-04-20 02:24:34 +0000

    Completed

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,47 @@
+class ZebraController < ApplicationController
+  def rules
+    render({ :template => "game_templates/rules" }) 
+  end
+  
+  def giraffe
+      moves = ["rock", "paper", "scissors"]
+      @comp_move = moves.sample
+
+      if @comp_move == "rock"
+        @outcome = "tied"
+      elsif @comp_move == "paper"
+        @outcome = "lost"
+      else
+        @outcome = "won"
+      end
+    render({ :template => "game_templates/play_rock" }) 
+  end
+
+  def elephant
+      moves = ["rock", "paper", "scissors"]
+      @comp_move = moves.sample
+      
+      if @comp_move == "rock"
+        @outcome = "won"
+      elsif @comp_move == "paper"
+        @outcome = "tied"
+      else
+        @outcome = "lost"
+      end
+    render({ :template => "game_templates/play_paper" }) 
+  end
+
+def lion
+      moves = ["rock", "paper", "scissors"]
+      @comp_move = moves.sample
+  
+      if @comp_move == "rock"
+        @outcome = "lost"
+      elsif @comp_move == "paper"
+        @outcome = "won"
+      else
+        @outcome = "tied"
+      end
+    render({ :template => "game_templates/play_scissors" }) 
+  end
+end
